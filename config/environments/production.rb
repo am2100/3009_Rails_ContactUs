fab422fbb87decc8288ec2744b437fd282af86bd
Jim Noble <jimnoble@xjjz.co.uk>
2012-02-22 15:16:22 +0000

    Correct typo.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ ContactUs::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Configure Heroku to use GMail
-  config.action_mailer.default_url_options = { :host => 'vivid-mist-6040.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'vivid-mist-6040.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
